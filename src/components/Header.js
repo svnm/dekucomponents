9621db5549a9764a5157de50bc501d66b7c3c19b
Steven Iseki Martin <stevenisekimartin@gmail.com>
2015-12-29 23:33:17 +1100

    add image

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,14 @@ function Header(props) {
   return (
     <div className={styles.header}>
 
-      <h3 className={styles.deku}>        
-          deku components
-      </h3>
+      <img className={styles.image} src='/public/deku-zelda.jpg' 
+           alt='copyright LinkofSkyWind http://linkofskywind.deviantart.com/art/Possessed-Wind-Link-and-Deku-Skyward-Link-347757288' 
+           title='LinkofSkyWind http://linkofskywind.deviantart.com/art/Possessed-Wind-Link-and-Deku-Skyward-Link-347757288' />
 
       <p className={styles.description}>
         Search for any deku components on npm... 
         make sure you include the 
-        <code className={styles.dark}>deku-component</code> 
+        <code className={styles.light}>deku-component</code> 
         keyword in your 
         <code className={styles.light}>package.json</code>
       </p>
